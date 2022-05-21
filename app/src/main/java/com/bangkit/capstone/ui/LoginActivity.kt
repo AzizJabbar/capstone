5a41f2fb5518760f126d9bfd1a0ce8e450668f9f
ATHALLAH RIKZA IHSANI <arikihsani@gmail.com>
2022-05-21 20:44:25 +0700

    creating login & register activity

--- app/src/main/java/com/bangkit/capstone/ui/LoginActivity.kt
+++ app/src/main/java/com/bangkit/capstone/ui/LoginActivity.kt
@@ -3,28 +3,31 @@ package com.bangkit.capstone.ui
 import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import com.bangkit.capstone.databinding.ActivityWelcomeBinding
+import com.bangkit.capstone.databinding.ActivityLoginBinding
 import com.bangkit.capstone.model.UserModel
 import com.bangkit.capstone.model.UserPreference
 
-class WelcomeActivity : AppCompatActivity() {
-    private lateinit var binding: ActivityWelcomeBinding
+class LoginActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityLoginBinding
     private lateinit var mUserPreference: UserPreference
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        binding = ActivityWelcomeBinding.inflate(layoutInflater)
+        binding = ActivityLoginBinding.inflate(layoutInflater)
         setContentView(binding.root)
+
         setupAction()
     }
 
     private fun setupAction(){
         binding.loginButton.setOnClickListener{
-            startActivity(Intent(this, LoginActivity::class.java))
+            // using dummy user for authentication
+            val dummyUser = UserModel("1", "dummyUser1", "token1")
+            mUserPreference = UserPreference(this)
+            mUserPreference.setUser(dummyUser)
+            startActivity(Intent(this, MainActivity::class.java))
+            finish()
         }
-        binding.registerButton.setOnClickListener{
-            startActivity(Intent(this, RegisterActivity::class.java))
-        }
-
     }
+
 }
